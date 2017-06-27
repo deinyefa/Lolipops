082e15fcfe60c15bb2a908a00692e4506a8e0f03
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-06-27 12:52:07 -0400

    timer.cs and a slider to introduce time limit

---
+++ Assets/Scripts/Extras/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+	private Slider silderObject;
+	public Image fillimg;
+
+	public float timeLimitInSeconds = 300;
+	private float timeLeft;
+
+	void Start () {
+		silderObject = GetComponent<Slider> ();
+
+		timeLeft = timeLimitInSeconds;
+		silderObject.maxValue = timeLimitInSeconds;
+	}
+	
+	void Update () {
+		timeLeft -= Time.deltaTime;
+		silderObject.value = timeLeft;
+	}
+
+	public void HalfwayThroughPlusMore (float value) {
+		if (value <= timeLimitInSeconds / 2) {
+			fillimg.color = Color.yellow;
+		}
+		if (value <= (timeLimitInSeconds / 4)) {
+			fillimg.color = Color.red;
+		}
+
+		if (value <= 0)
+			SceneManager.LoadScene ("MainMenu");
+	}
+}
